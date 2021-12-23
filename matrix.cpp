afad0329fa00c173f1b1899712661924fdc3054f
Looka997 <luca.marinelli997@gmail.com>
2021-12-23 13:16:23 +0100

    implements exercise 1 and 2

--- matrix.cpp
+++ matrix.cpp
@@ -2,45 +2,59 @@
 #include "matrixFactory.h"
 
 using namespace std;
+typedef float num;
+void workMatrix(matrix<num>&);
 int main(){
-    typedef float num;
+
     int const d0 = 6, d1=0;
     int n = 10 * (d1 + 1) + d0;
 
     matrixFactory mf = matrixFactory();
 #ifdef TRIANG
     matrix<num> tri = mf.Triangular(n);
-    tri.print();
+    cout << "with matrix tri:" << endl;
+    workMatrix(tri);
 #endif
 #ifdef A
     matrix<num> a = mf.SpecificA();
-    a.print();
+    cout << "with matrix A:" << endl;
+    workMatrix(a);
 #endif
 #ifdef B
     matrix<num> b = mf.SpecificB();
-    b.print();
+    cout << "with matrix B:" << endl;
+    workMatrix(b);
 #endif
 #ifdef PASCAL
     matrix<num> pascal = mf.Pascal(10);
-    pascal.print();
+    cout << "with matrix pascal:" << endl;
+    workMatrix(pascal);
 #endif
-    number* mat = new number[20]{0, 0, 1, 0, 3, 1, 3, 2, 1, 0, 4, 1, 0, 0, 6, 2, 0, 5, 4, 9};
-    matrix<num> ma (4,5, mat);
-//    ma.print();
-#ifdef GAUSS
-//    ma.gauss();
 
-#ifndef SHOW_CANCELLATION
-//    ma.print();
-#endif
-#endif
 //    number* s2 = new number[4]{1,1,1,1};
 //    a.addSolutions(s2);
 //    delete [] s2;
-    cout << a.infinityNorm() << endl;
-    cout << b.infinityNorm() << endl;
-    matrix<num>prod = a * b;
-    prod.print();
-    cout << prod.infinityNorm() << endl;
     return 1;
+}
+
+void workMatrix(matrix<num> &a){
+    a.print();
+
+    /* Esercizio 1 */
+    cout << "Norma infinito A: " << a.infinityNorm() << endl;
+
+    /* Esercizio 2 */
+    matrix<num> x(a.getRows(), 1);
+    x.init(1);
+    matrix<num> b = a * x;
+    cout << "Vettore b generato:" << endl;
+    b.print();
+    a.addSolutions(b);
+    a.print();
+    a.gauss();
+#ifndef SHOW_CANCELLATION
+    a.print();
+#endif
+
+
 }
